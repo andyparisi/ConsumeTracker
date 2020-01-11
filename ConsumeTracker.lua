9c229568b023a2760ca338512988f95881e52ee9
Andrew Parisi <andrew.parisi8@gmail.com>
2020-01-11 15:36:35 -0500

    fixes

--- ConsumeTracker.lua
+++ ConsumeTracker.lua
@@ -1,30 +1,40 @@
 ConsumeTracker = { }
-
-function ConsumeTracker:OnInitialize()
-    ClassicGuildBank:RegisterChatCommand('contra', 'ShowMainFrame')
-    ContraFrameHideButton:SetScript("OnClick", function(self)
-        ContraFrame:Hide()
-        end
-    )
-    ContraFrameStartButton:SetScript("OnClick", function(self))
-        self:StartTracking()
-        end
-    )
-    ContraFrameStopButton:SetScript("OnClick", function(self))
-        self:StopTracking()
-        end
-    )
-end
+SLASH_CONSUMETRACKER1 = '/contra'
 
 function ConsumeTracker:ShowMainFrame()
     ContraFrame:Show()
 end
 
 function ConsumeTracker:StartTracking()
-    print("Start")
-    
+    ContraFrame:SetScript("OnEvent", function(self, event, ...)
+        if event == "UNIT_AURA" then
+            print("AURA" .. event) 
+            end
+        end
+    )    
 end
 
 function ConsumeTracker:StopTracking()
     print("Stop")
 end
+
+function SlashCmdList.CONSUMETRACKER(msg, editBox) -- 4.
+    ConsumeTracker:ShowMainFrame()
+end
+
+function ConsumeTracker:Initialize()
+    ContraFrameHideButton:SetScript("OnClick", function(self)
+        ContraFrame:Hide()
+        end
+    )
+    ContraFrameStartButton:SetScript("OnClick", function(self)
+        ConsumeTracker:StartTracking()
+        end
+    )
+    ContraFrameStopButton:SetScript("OnClick", function(self)
+        ConsumeTracker:StopTracking()
+        end
+    )
+end
+
+ConsumeTracker:Initialize()
